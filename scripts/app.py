d75120ba901ac705a927785065ceda682fc701cd
NikhilJuneja9 <nikhiljuneja115@gmail.com>
2024-07-23 22:50:16 +0530

    [ADD-ABS_Path

--- scripts/app.py
+++ scripts/app.py
@@ -1,10 +1,13 @@
 import streamlit as st
 import pickle
 import numpy as np
+import os
 
 # import the model
-pipe = pickle.load(open('scripts\pipe.pkl','rb'))
-df = pickle.load(open('scripts\df.pkl','rb'))
+pipe_path = os.path.join(os.path.dirname(__file__),  'pipe.pkl')
+df_path = os.path.join(os.path.dirname(__file__),  'df.pkl')
+pipe = pickle.load(open(pipe_path,'rb'))
+df = pickle.load(open(df_path,'rb'))
 
 st.title("Laptop Predictor")
 
@@ -42,7 +45,7 @@ ssd = st.selectbox('SSD(in GB)',[0,8,128,256,512,1024])
 
 gpu = st.selectbox('GPU',df['Gpu Brand'].unique())
 
-os = st.selectbox('OS',df['Operating Sys'].unique())
+Opr = st.selectbox('OS',df['Operating Sys'].unique())
 
 if st.button('Predict Price'):
     ppi = None
@@ -60,7 +63,7 @@ if st.button('Predict Price'):
     X_res = int(resolution.split('x')[0])
     Y_res = int(resolution.split('x')[1])
     ppi = ((X_res**2) + (Y_res**2))**0.5/screen_size
-    query = np.array([company,type,ram,weight,size,touchscreen,ips,ppi,cpu,hdd,ssd,gpu,os])
+    query = np.array([company,type,ram,weight,size,touchscreen,ips,ppi,cpu,hdd,ssd,gpu,Opr])
 
     query = query.reshape(1,13)
     st.title("The predicted price of this configuration is " + str(int(np.exp(pipe.predict(query)[0]))))
